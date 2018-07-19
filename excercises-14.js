43de5f277533985b7a362c090ff216252a85e81d
Anis Fikriyyah <38931702+anisfikriyyah@users.noreply.github.com>
2018-07-19 18:43:30 +0700

    Add files via upload

---
+++ excercises-14.js
@@ -0,0 +1,37 @@
+function sorting(arrNumber) {
+    // code di sini
+   arrNumber.sort(function(a, b){return a<b})
+    return arrNumber
+  }
+  
+  function getTotal(arrNumber) {
+    // code di sini
+    var tamp = 1
+    for ( var i=1; i<arrNumber.length; i++){
+      // console.log(arrNumber[i])
+      if (arrNumber[i] === arrNumber[0]){
+        tamp++
+      } 
+    }
+       if (arrNumber.length === 0) {
+          return ''
+    }
+    return 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculan sebanyak ' + tamp + ' kali'
+  }
+  
+  function mostFrequentLargestNumbers(arrNumber) {
+    var listSort = sorting(arrNumber);
+    var countHighest = getTotal(listSort);
+    return countHighest;
+  }
+  
+  console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+  //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+  
+  console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+  //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+  
+  console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+  //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+  
+  console.log(mostFrequentLargestNumbers([]));
